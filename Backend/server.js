68548851d5c39b928f4c0ee59430f9bdd39bdeec
Suvrat1629 <suvrat1629@gmail.com>
2025-05-12 20:02:26 +0530

    helo

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'https://task-management-nine-beige.vercel.app/',
+  origin: 'https://task-management-nine-beige.vercel.app',
   credentials: true
 }));
 
